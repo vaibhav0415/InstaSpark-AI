f06aa47abd8b8b5ca960fd20c6daba542e7c3287
vaibhav0415 <vv944445@gmail.com>
2026-06-27 15:54:49 +0530

    Initial Project Setup

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+typescriptimport { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+});
